7aaa85ed5b5eaef9d254b3130f04c3310541c036
montgomeri15 <montgomeri15@gmail.com>
2022-10-21 17:14:13 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    //Просте число — це натуральне число, яке має рівно два різних натуральних дільники (лише 1 і саме число)
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
