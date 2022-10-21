9c62570e17e1a707dabe66d30e7a3994551fa8ae
montgomeri15 <montgomeri15@gmail.com>
2022-10-21 18:17:00 +0300

    With a prime number recursion

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,25 @@
-public class Main {
-    //Просте число — це натуральне число, яке має рівно два різних натуральних дільники (лише 1 і саме число)
+import java.util.List;
 
+public class Main {
+    //Просте число — це натуральне число, яке ділиться на 1 і саме себе
+    private static int i = 2;
+
+    public static boolean isPrime(int num){
+        if(num > 2 && i == num / 2 + 1){
+            return true;
+        } else if(num % i == 0){
+            return false;
+        }
+        i++;
+        return isPrime(num);
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int number = 6;
+        if(isPrime(number) == true){
+            System.out.println(number + "\t - просте число");
+        } else {
+            System.out.println(number + "\t - не просте число");
+        }
     }
 }
